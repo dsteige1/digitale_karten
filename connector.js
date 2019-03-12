316178f14858a8002168e1c0c4b02d80ebfed22b
Ibrahim Tuna <ibrahim.tuna@web.de>
2019-03-12 20:41:47 +0100

    Slider mit Datenbank verknüpft, überblick Funktionen hinzugefügt

--- connector.js
+++ connector.js
@@ -1,31 +1,13 @@
-var data;
-
-$.ajax({
-    url:        'data/datenbank.json',
-    dataType:   'json',
-    type:       'get',
-    cache:      'true',
-    success:    function(data){
-
-        console.log('Data loaded');
-        /*
-        for (let i=2; i<=5; i++)
-        {
-            console.log(
-                "Event: " +
-                data.datenbank.event[i].veranstaltung.name + "\n" +
-                "Datum: " +
-                data.datenbank.event[i].veranstaltung.daten.datum + "\n" +
-                "Location: " +
-                data.datenbank.event[i].location.gaststaette + "\n" +
-                "Latitude: " + 
-                data.datenbank.event[i].location.lat + "\n" +
-                "Longitude: " + 
-                data.datenbank.event[i].location.long + "\n" +
-                "Teilnehmende: " +
-                data.datenbank.event[i].veranstaltung.teilnehmerzahl.teilgenommen + "\n"
-            )
+var data = (function () {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': 'data/datenbank.json',
+        'dataType': "json",
+        'success': function (jsondata) {
+            data = jsondata;
         }
-        */
-    }
-})
+    });
+    return data;
+})();
