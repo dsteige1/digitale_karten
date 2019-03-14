d92411a74a985dbb81072b59d1c55e353c8ecc82
David <david.j.steiger@web.de>
2019-03-14 11:28:01 +0100

    Marker-Größe relativ zur Teilnehmer/innen-Zahl. Ordner-Struktur eingerichtet.

---
+++ javascript/connector.js
@@ -0,0 +1,13 @@
+var data = (function () {
+    var json = null;
+    $.ajax({
+        'async': false,         //throws error:
+        'global': false,
+        'url': '../data/datenbank.json',
+        'dataType': "json",
+        'success': function (jsondata) {
+            data = jsondata;
+        }
+    });
+    return data;
+})();
